c1f2f9420ab97ad12a5c1424f698629e0bb87c13
boualavong <boualavong.celine@gmail.com>
2017-02-26 23:23:12 +0100

    Projet Disnyabolik-Alice In Wonderland
    Premier niveau de l'événement "Disnyabolik", un jeu d'énigmes en lien
    avec l'univers Disney.

---
+++ Disnyabolik-Alice In Wonderland/Le roi Lion/Le roi lion/Formulaire2.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="guess?">
+    
+    <title>Level 1 - Le Roi Lion</title>
+    
+    <link href="form.css" media="screen" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="img/footer-logo.png" />
+
+
+</head>
+<body onload="document.forms[0].pw.focus()" bgcolor="black">
+<center>
+  <img src="img/smile.jpg"/>
+  <form id="password" action="enigme2.html" method="POST">
+    <!-- remplacer enigme 1 par le php -->
+  <input type="password" name="password" id="textfield" maxlength="21"></p><br />
+  <input type="image" src="img/enter_button.png" onMouseOver="this.src='img/enter_button_hover.png'" onMouseOut="this.src='img/enter_button.png'" alt="Enter">
+  </form>
+</center>
+</body>
+</html>
